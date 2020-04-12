2cc05d78601d138197651728f6bcce9c5de7c601
filippo <filippo.guggino@gmail.com>
2020-04-12 14:04:00 +0200

    Pushed working getRanking.

--- src/main/java/com/task2_3/server/MongoDBManager.java
+++ src/main/java/com/task2_3/server/MongoDBManager.java
@@ -6,10 +6,7 @@ import com.mongodb.client.model.Aggregates;
 import org.bson.BsonDocument;
 import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Projections.include;
@@ -62,44 +59,81 @@ MongoDatabase database = mongoClient.getDatabase("test");
     public void getRanking(){
         MongoDatabase database = mongoClient.getDatabase("us_flights_db");
         MongoCollection<Document> collection = database.getCollection("us_flights");
-        HashMap<RankKey, Integer> ranking = new HashMap<>();
+        HashMap<Integer, Vector<RankClass>> ranking = new HashMap<>();
 
 
-        try(MongoCursor<Document> cursor = collection.find().sort(orderBy(ascending("DEST_AIRPORT.DEST_AIRPORT_ID", "OP_UNIQUE_CARRIER"))).projection(include("DEST_AIRPORT.DEST_AIRPORT_ID","OP_UNIQUE_CARRIER")).iterator()) {
+        //TODO choose if cursor or not, this solution is not MEMORY SAFE and slower than the equivalent with cursors, for testing remove Accumulators.push https://stackoverflow.com/questions/44587829/sorting-and-ranking-documents-based-on-key-values
+        //if this solution is chosen delete RankClass and RankKey
+        try(MongoCursor<Document> cursor = collection.aggregate(
+                Arrays.asList(
+                        Aggregates.group(new Document("DEST_AIRPORT", "$DEST_AIRPORT.DEST_AIRPORT_ID").append("OP_UNIQUE_CARRIER", "$OP_UNIQUE_CARRIER"), Accumulators.sum("count", 1)/*, Accumulators.push("items", new Document("push", "$$ROOT"))*/),
+                        Aggregates.sort(orderBy(ascending("_id.DEST_AIRPORT"), descending("count")))
+                        )
+             ).iterator())
+        {
+            Document doc;
+            Document id;
+            int rank = 1;
+            int current_airport = 0;
+
+            while (cursor.hasNext()) {
+                doc = (Document) cursor.next();
+                id = (Document) doc.get("_id");
+
+                String ind_carrier = id.getString("OP_UNIQUE_CARRIER" );
+                int ind_airport = id.getInteger("DEST_AIRPORT" );
+                rank++;
+                if(current_airport != ind_airport) {
+                    rank = 1;
+                    current_airport = ind_airport;
+                }
+                System.out.println(ind_airport + "              " + ind_carrier + "            " + doc.getInteger("count") + "          " + rank);
+            }
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //TODO this solution is better but doesn't exploit aggregations
+        /*try(MongoCursor<Document> cursor = collection.find().sort(orderBy(ascending("DEST_AIRPORT.DEST_AIRPORT_ID", "OP_UNIQUE_CARRIER"))).projection(include("DEST_AIRPORT.DEST_AIRPORT_ID","OP_UNIQUE_CARRIER")).iterator()) {
             Document doc = cursor.next();
             Document airport = (Document) doc.get("DEST_AIRPORT");
 
-            Double current_airport = airport.getDouble("DEST_AIRPORT_ID");
+            int current_airport = airport.getInteger("DEST_AIRPORT_ID");
             String current_carrier = doc.getString("OP_UNIQUE_CARRIER");
-            int rank = 1;
+            int num_flight = 1;
+
+            Vector<RankClass> rank_arr = new Vector<>();
 
             while (cursor.hasNext()) {
                 doc = cursor.next();
                 String ind_carrier = doc.getString("OP_UNIQUE_CARRIER");
                 airport = (Document) doc.get("DEST_AIRPORT");
-                Double ind_airport = airport.getDouble("DEST_AIRPORT_ID");
+                int ind_airport = airport.getInteger("DEST_AIRPORT_ID");
 
-                if(!ind_airport.equals(current_airport) || !ind_carrier.equals(current_carrier)/* || current_airport == null || current_carrier == null*/){
-                    ranking.put(new RankKey(current_airport, current_carrier), rank);
+                if(ind_airport != current_airport || !ind_carrier.equals(current_carrier)){
+                    //ranking.put(new RankKey(current_airport, current_carrier), rank);
 
-                    rank = 0;
+                    rank_arr.add(new RankClass(current_carrier, RankClass.last_rank, num_flight));
+                    ranking.put(current_airport, rank_arr);
+
+                    num_flight = 0;
                     current_airport = ind_airport;
                     current_carrier =  ind_carrier;
-
                 }
                 rank++;
             }
+            //add last one
             ranking.put(new RankKey(current_airport, current_carrier), rank);
 
             for(Map.Entry<RankKey, Integer> entry: ranking.entrySet()){
-                double airport_key = entry.getKey().getAirport();
+                int airport_key = entry.getKey().getAirport();
                 String carrier_key = entry.getKey().getCarrier();
                 int rank_val = entry.getValue();
                 System.out.println("aereoport: "+ airport_key + " carrier: "+carrier_key+" numero voli: "+rank_val);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
 }
 
