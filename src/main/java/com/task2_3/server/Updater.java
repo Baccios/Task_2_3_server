87eb87b85d238c8ee8b5f2af9e5d5f3221d4fd6b
zaccaria-essaid <zaccaria-essaid@hotmail.it>
2020-04-26 17:08:59 +0200

    Updater added.

---
+++ src/main/java/com/task2_3/server/Updater.java
@@ -0,0 +1,18 @@
+package com.task2_3.server;
+
+import java.util.TimerTask;
+
+public class Updater extends TimerTask {
+    private MongoDBManager mongomanager;
+    private Neo4jDBManager neomanager;
+
+    public Updater(MongoDBManager mongomanager,Neo4jDBManager neomanager){
+        this.mongomanager=mongomanager;
+        this.neomanager=neomanager;
+    }
+    public void run(){
+        UpdatePacket updatePacket=mongomanager.getUpdatePacket();
+        neomanager.update(updatePacket);
+
+    }
+}
